804c26d2cb9d3c9e6d495dcf7580504a88693458
Patryk Szober <pszober@cogitech.pl>
2020-12-04 19:11:36 +0100

    -minit project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <Navbar />
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -8,6 +9,13 @@
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar'
+export default {
+  name: 'App',
+  components: { Navbar },
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
